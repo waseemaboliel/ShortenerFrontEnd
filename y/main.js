0f2da167913ad17e9157a750321f2ef95516206d
Waseem Aboliel <waseem.aboliel@gmail.com>
2024-01-17 01:00:35 +0200

    Deploying to Firebase

--- y/main.js
+++ y/main.js
@@ -1,6 +1,6 @@
 var GETdata;
 var POSTdata;
-const SERVER_URL = 'http://localhost:5000';
+const SERVER_URL = 'http://localhost:8080';
 
 function validURL(str) {
     var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
